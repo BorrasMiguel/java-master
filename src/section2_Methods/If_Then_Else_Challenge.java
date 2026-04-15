e7ac0c2182263481bcd9bf9cf578f5c1397c6ce9
Miguel Borrás <miguelborras94@gmail.com>
2026-04-15 19:19:29 +0200

    Methods

---
+++ src/section2_Methods/If_Then_Else_Challenge.java
@@ -0,0 +1,49 @@
+package section2_Methods;
+
+public class If_Then_Else_Challenge {
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int finalScore = score;
+
+        calculateScore(true, 800, 5, 100);
+
+//        boolean newGameOver = true;
+//        int newScore = 10000;
+//        int newLevelCompleted = 8;
+//        int newBonus = 200;
+//
+//        int newFinalScore = newScore;
+//
+//        if (newGameOver) {
+//            newFinalScore += (newLevelCompleted * newBonus);
+//            System.out.println("Your final score is " + newFinalScore);
+//        }
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score is " + finalScore);
+        }
+
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        int finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score is " + finalScore);
+        }
+    }
+}
